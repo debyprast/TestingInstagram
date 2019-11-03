9f3dfb33209db993481c54fc939737cf677ddd10
debyprast <debyprast18@gmail.com>
2019-11-04 02:18:24 +0700

    Membuat class testing buka web IG dan login IG

---
+++ TestingInstagram/src/Instagram/BukaWeb.java
@@ -0,0 +1,18 @@
+package Instagram;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BukaWeb {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver() ;
+		driver.get("http://www.instagram.com/") ;
+		driver.manage().window().maximize();
+		
+	}
+
+}
